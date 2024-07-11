8e39c919e4b5a2b3bb293763f9dc97fe8c962bbb
Filip Jonasson <jonassonfilip@gmail.com>
2024-07-11 02:53:54 +0200

    Updated tables

--- src/app/page.js
+++ src/app/page.js
@@ -46,62 +46,66 @@ export default function Home() {
             ))}
           </div>
           <div className={styles.table}>
-            <h2>Tabell 7/7:</h2>
+            <h2>Tabellen inför finalen:</h2>
             <div className={styles.tableRows}>
               <div className={styles.tableRow}>
                 <ul>
-                  <h4>285 poäng:</h4>
+                  <h4>300 poäng:</h4>
                   <li>Anncatrine</li>
 
                   <h4>280 poäng:</h4>
                   <li>Joakim</li>
-
-                  <h4>265 poäng:</h4>
                   <li>Linus</li>
+
+                  <h4>270 poäng:</h4>
+                  <li>Peter</li>
 
                   <h4>260 poäng:</h4>
                   <li>Jakob</li>
-
-                  <h4>255 poäng:</h4>
-                  <li>Peter</li>
-                  <li>Therese</li>
                 </ul>
               </div>
               <div className={styles.tableRow}>
                 <ul>
-                  <h4>245 poäng:</h4>
-                  <li>Johan</li>
-                  <li>Ludwig P</li>
-
-                  <h4>240 poäng:</h4>
+                  <h4>255 poäng:</h4>
                   <li>Carolina</li>
                   <li>Henrik</li>
                   <li>Robin</li>
+                  <li>Therese</li>
 
-                  <h4>230 poäng:</h4>
+                  <h4>245 poäng:</h4>
                   <li>Basta</li>
-                  <li>Elias</li>
-                  <li>Filip</li>
+                  <li>Johan</li>
+                  <li>Ludwig P</li>
                   <li>Ludwig R</li>
                 </ul>
               </div>
               <div className={styles.tableRow}>
                 <ul>
-                  <h4>220 poäng:</h4>
-                  <li>Marcus</li>
+                  <h4>235 poäng:</h4>
                   <li>Victor</li>
 
+                  <h4>230 poäng:</h4>
+                  <li>Elias</li>
+                  <li>Filip</li>
+
+                  <h4>225 poäng:</h4>
+                  <li>Karl</li>
+
+                  <h4>220 poäng:</h4>
+                  <li>Marcus</li>
+                </ul>
+              </div>
+
+              <div className={styles.tableRow}>
+                <ul>
                   <h4>215 poäng:</h4>
                   <li>Anders</li>
 
-                  <h4>210 poäng:</h4>
-                  <li>Karl</li>
+                  <h4>205 poäng:</h4>
+                  <li>Alessandro</li>
 
                   <h4>200 poäng:</h4>
                   <li>Glenn</li>
-
-                  <h4>190 poäng:</h4>
-                  <li>Alessandro</li>
                 </ul>
               </div>
             </div>
